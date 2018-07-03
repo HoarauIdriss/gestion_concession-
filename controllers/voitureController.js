a0d6a3f8c2ef9dd3f7a6f2ed864f9d0d7a246856
Lisa9740 <barretalison@gmail.co>
2018-07-03 10:14:22 +0400

    Ajout verification champs vide dans le back

--- controllers/voitureController.js
+++ controllers/voitureController.js
@@ -10,26 +10,26 @@ var voitureController = {};
 //----------------------
 
 //Lister les voitures et les affiche dans la page d'accueil
-voitureController.listaccueil = function(req, res) {
-    Voiture.find({}).exec(function(err, voitures){
-        if(err){
+voitureController.listaccueil = function (req, res) {
+    Voiture.find({}).exec(function (err, voitures) {
+        if (err) {
             console.log('Error : ', err);
-        }else{
-            res.render("index",{voitures:voitures} );
-        } 
+        } else {
+            res.render("index", { voitures: voitures });
+        }
     });
 };
 
 
 
 //Affiche une voiture par rapport à son ID
-voitureController.show = function(req, res) {
-    Voiture.findOne({_id:req.params.id}).exec(function(err, voiture){
-        if(err){
+voitureController.show = function (req, res) {
+    Voiture.findOne({ _id: req.params.id }).exec(function (err, voiture) {
+        if (err) {
             console.log('Error : ', err);
-        }else{
-            res.render("../views/voiture/viewdetails",{voiture:voiture});
-        } 
+        } else {
+            res.render("../views/voiture/viewdetails", { voiture: voiture });
+        }
     });
 };
 
@@ -38,68 +38,109 @@ voitureController.show = function(req, res) {
 //----------------------
 
 //Lister les voitures et les affiche dans l'index du Back office
-voitureController.list = function(req, res) {
-    Voiture.find({}).exec(function(err, voitures){
-        if(err){
+voitureController.list = function (req, res) {
+    Voiture.find({}).exec(function (err, voitures) {
+        if (err) {
             console.log('Error : ', err);
-        }else{
-            res.render("../views/voiture/admin/index",{voitures:voitures} );
-        } 
+        } else {
+            res.render("../views/voiture/admin/index", { voitures: voitures });
+        }
     });
 };
 //Redirection vers la page de creation d'une voiture
-voitureController.create = function(req, res){
+voitureController.create = function (req, res) {
     res.render("../views/voiture/admin/create");
-}; 
+};
+// fonction de vérification des données vides
+ function verifChampVide(champs){
+
+    var condition =true ;
+    for (var i = 0 ; i < champs.length ; i++){
+        if ( champs[i] == "" ) {
+            condition = false ;
+        }
+        }
+        return condition
+
+}
 
 //Enregistrement d'une voiture
-voitureController.save = function(req, res){
+voitureController.save = function (req, res) {
     var voiture = new Voiture(req.body);
+    var info = [req.body.marque, 
+        req.body.modele, 
+        req.body.image, 
+        req.body.puissance, 
+        req.body.motorisation,
+        req.body.prix, 
+        req.body.couleur, 
+        req.body.portes, 
+        req.body.places, 
+        req.body.actif]
 
-    voiture.save(function(err){
-        if(err){
-            console.log(err);
+    var verif= verifChampVide(info)
+     
+        if (verif == true){ 
+
+            voiture.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.render("../views/voiture/admin/create");
+                } else {
+                    console.log("creation voiture OK");
+                    res.redirect("/voitures/admin");
+                }
+            });
+    
+        }else {
             res.render("../views/voiture/admin/create");
-        } else{
-            console.log("creation voiture OK");
-            res.redirect("/voitures/admin");
-        } 
-    });
+
+        }
+    
+    
+
+   
+
 };
 
+
+
 //Edition d'un voiture par son id
-voitureController.edit = function(req, res){
+voitureController.edit = function (req, res) {
     var voiture = new Voiture(req.body);
 
-    Voiture.findOne({_id:req.params.id}).exec(function(err, voiture){
-        if(err){
+    Voiture.findOne({ _id: req.params.id }).exec(function (err, voiture) {
+        if (err) {
             console.log("Error ", err);
-        } else{
-            res.render("../views/voiture/admin/edit",{voiture: voiture} );
-        } 
+        } else {
+            res.render("../views/voiture/admin/edit", { voiture: voiture });
+        }
     });
 };
 
 //Gestion de l'edition dun voiture
-voitureController.update = function(req, res){
-    Voiture.findByIdAndUpdate(req.params.id,{ $set :{
-        marque: req.body.marque, 
-        modele: req.body.modele, 
-        image: req.body.image, 
-        puissance: req.body.puissance, 
-        motorisation: req.body.motorisation, 
-        prix: req.body.prix, 
-        couleur: req.body.couleur, 
-        portes: req.body.portes, 
-        places: req.body.places, 
-        actif: req.body.actif} },{new: true}, function (err, voiture){
+voitureController.update = function (req, res) {
+    Voiture.findByIdAndUpdate(req.params.id, {
+        $set: {
+            marque: req.body.marque,
+            modele: req.body.modele,
+            image: req.body.image,
+            puissance: req.body.puissance,
+            motorisation: req.body.motorisation,
+            prix: req.body.prix,
+            couleur: req.body.couleur,
+            portes: req.body.portes,
+            places: req.body.places,
+            actif: req.body.actif
+        }
+    }, { new: true }, function (err, voiture) {
 
-        if (err){
+        if (err) {
             console.log(err);
-            res.render("../views/voiture/admin/edit",{voiture:req.body} );
-        } 
+            res.render("../views/voiture/admin/edit", { voiture: req.body });
+        }
         res.redirect("/voitures/admin");
-        
+
     });
 };
 
